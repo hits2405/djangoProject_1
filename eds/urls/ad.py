98f7a47ccbdbb6c4cb92e60d67ab59acc7a72f89
hits2405 <hits13@mail.ru>
2022-12-29 19:17:04 +0300

    Initial commit

---
+++ eds/urls/ad.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from eds.views import AdLCV, AdDV
+
+urlpatterns = [
+    path('', AdLCV.as_view()),
+    path('<int:pk>', AdDV.as_view())
+]
---
+++ eds/urls/cat.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from eds.views import root
+
+from eds.views import CatDV, CatLCV
+
+urlpatterns = [
+    path('', CatLCV.as_view()),
+    path('<int:pk>', CatDV.as_view())
+]
