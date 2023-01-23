277ea11fab5c57e59ef77d8de17fffea763f2231
hits2405 <hits13@mail.ru>
2023-01-23 20:29:11 +0300

    add urlpatterns from ad/ and cat/

--- eds/urls/ad.py
+++ eds/urls/ad.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from eds.views.ad import AdLCV, AdDV
+from eds.views.ad import *
 
 urlpatterns = [
     path('', AdLCV.as_view()),
-    path('<int:pk>/', AdDV.as_view())
+    path('<int:pk>/', AdDV.as_view()),
+    path('create/', AdCV.as_view()),
+    path('<int:pk>/upload_image/', AdUpIm.as_view()),
+    path('<int:pk>/update/', AdUV.as_view()),
+    path('<int:pk>/delete/', AdDeleteV.as_view()),
 ]
--- eds/urls/cat.py
+++ eds/urls/cat.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from eds.views.cat import root
 
-from eds.views.cat import CatDV, CatLV
+from eds.views.cat import *
 
 urlpatterns = [
     path('', CatLV.as_view()),
-    path('<int:pk>/', CatDV.as_view())
+    path('<int:pk>/', CatDV.as_view()),
+    path('create/', CatCV.as_view()),
+    path('<int:pk>/update/', CatUV.as_view()),
+    path('<int:pk>/delete/', CatDeleteV.as_view()),
 ]
